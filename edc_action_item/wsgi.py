29106232068aa718221c37c38917ec9a18ed35f8
erikvw <ew2789@gmail.com>
2018-07-07 18:52:48 +0200

    minor

--- edc_action_item/wsgi.py
+++ edc_action_item/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for edc_action_items project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
