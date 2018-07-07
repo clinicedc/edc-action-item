224c0923d63912585608e03a1f5e688e9c84f825
erikvw <ew2789@gmail.com>
2018-07-07 18:49:22 +0200

    fix urls, add link to related reference

--- edc_action_item/templatetags/action_item_extras.py
+++ edc_action_item/templatetags/action_item_extras.py
@@ -1,5 +1,4 @@
 from django import template
-from django.apps import apps as django_apps
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from edc_base.utils import convert_php_dateformat
@@ -33,6 +32,7 @@ def action_item_with_popover(action_item_model_wrapper, tabindex):
     date_format = convert_php_dateformat(settings.SHORT_DATE_FORMAT)
 
     if action_item.last_updated:
+        # could also use action_item.linked_to_reference?
         last_updated = action_item.last_updated.strftime(date_format)
         user_last_updated = action_item.user_last_updated
         last_updated_text = (
@@ -40,70 +40,98 @@ def action_item_with_popover(action_item_model_wrapper, tabindex):
     else:
         last_updated_text = 'This action item has not been updated.'
 
-    # this reference model and url
-    reference_model_cls = django_apps.get_model(action_item.action_type.reference_model)
     query_dict = dict(parse_qsl(urlparse(href).query))
-    parent_reference_model_url = None
+    related_reference_url = None
+    related_reference_model_name = None
+    related_reference_identifier = None
+    parent_reference_url = None
+    parent_reference_identifier = None
     parent_reference_model_name = None
     action_item_reason = None
-    parent_action_identifier = None
-    # reference_model and url
-    action_cls = site_action_items.get(reference_model_cls.action_name)
+
+    # action class
+    action_cls = site_action_items.get(
+        action_item.reference_model_cls.action_name)
+
+    # reference_model instance
     try:
-        reference_model_obj = reference_model_cls.objects.get(
+        reference_obj = action_item.reference_model_cls.objects.get(
             action_identifier=action_item.action_identifier)
     except ObjectDoesNotExist:
-        reference_model_obj = None
+        reference_obj = None
+
+    # reference model url
     try:
-        reference_model_url = action_cls.reference_model_url(
+        reference_url = action_cls.reference_url(
             action_item=action_item,
             action_identifier=action_item.action_identifier,
-            reference_model_obj=reference_model_obj,
-            ** query_dict)
+            reference_obj=reference_obj,
+            **query_dict)
     except ObjectDoesNotExist:
-        reference_model_url = None
-        # object wont exist if an action item was deleted
+        reference_url = None
+        # object wont exist if action item was deleted
         # that was created by another action item.
         strike_thru = True
-    else:
-        if action_item.parent_action_item:
 
-            # parent action item
-            parent_reference_model_cls = django_apps.get_model(
-                action_item.parent_action_item.action_type.reference_model)
-
-            # parent reference model and url
+    if action_item.parent_reference_identifier:
+        # parent reference model and url
+        try:
+            parent_reference_obj = action_item.parent_reference_obj
+        except ObjectDoesNotExist:
+            pass
+        else:
             try:
-                parent_reference_model_obj = parent_reference_model_cls.objects.get(
-                    tracking_identifier=action_item.parent_action_item.reference_identifier)
-            except ObjectDoesNotExist:
+                subject_visit = parent_reference_obj.visit
+            except (AttributeError, ObjectDoesNotExist):
                 pass
             else:
-                try:
-                    subject_visit = parent_reference_model_obj.visit
-                except (AttributeError, ObjectDoesNotExist):
-                    pass
-                else:
-                    # parent reference model is a CRF, add visit to querystring
-                    query_dict.update({
-                        parent_reference_model_obj.visit_model_attr(): str(subject_visit.pk),
-                        'appointment': str(subject_visit.appointment.pk)})
-                parent_reference_model_url = (
-                    action_cls.reference_model_url(
-                        reference_model_obj=parent_reference_model_obj,
-                        action_item=action_item,
-                        action_identifier=action_item.action_identifier,
-                        **query_dict))
+                # parent reference model is a CRF, add visit to querystring
+                query_dict.update({
+                    parent_reference_obj.visit_model_attr(): str(subject_visit.pk),
+                    'appointment': str(subject_visit.appointment.pk)})
+            parent_reference_url = (
+                action_cls.reference_url(
+                    reference_obj=parent_reference_obj,
+                    action_item=action_item,
+                    action_identifier=action_item.action_identifier,
+                    **query_dict))
 
-                parent_reference_model_name = (
-                    f'{parent_reference_model_cls._meta.verbose_name} '
-                    f'{parent_reference_model_obj.tracking_identifier}')
-                action_item_reason = parent_reference_model_obj.action_item_reason
-            parent_action_identifier = action_item.parent_action_item.action_identifier
+            parent_reference_model_name = (
+                f'{action_item.parent_reference_model_cls._meta.verbose_name} '
+                f'{str(parent_reference_obj)}')
+            action_item_reason = parent_reference_obj.action_item_reason
+        parent_reference_identifier = action_item.parent_action_item.action_identifier
+
+    if action_item.related_reference_identifier:
+        try:
+            related_reference_obj = action_item.related_reference_obj
+        except ObjectDoesNotExist:
+            pass
+        else:
+            try:
+                subject_visit = related_reference_obj.visit
+            except (AttributeError, ObjectDoesNotExist):
+                pass
+            else:
+                # related reference model is a CRF, add visit to querystring
+                query_dict.update({
+                    related_reference_obj.visit_model_attr(): str(subject_visit.pk),
+                    'appointment': str(subject_visit.appointment.pk)})
+            related_reference_url = (
+                action_cls.reference_url(
+                    reference_obj=related_reference_obj,
+                    action_item=action_item,
+                    action_identifier=action_item.action_identifier,
+                    **query_dict))
+
+            related_reference_model_name = (
+                f'{action_item.related_reference_obj._meta.verbose_name} '
+                f'{str(related_reference_obj)}')
+        related_reference_identifier = action_item.related_reference_identifier
 
     open_display = [c[1] for c in ACTION_STATUS if c[0] == OPEN][0]
 
-    return dict(
+    context = dict(
         HIGH_PRIORITY=HIGH_PRIORITY,
         OPEN=open_display,
         action_instructions=action_item.instructions,
@@ -112,23 +140,28 @@ def action_item_with_popover(action_item_model_wrapper, tabindex):
         display_name=action_item.action_type.display_name,
         action_identifier=action_item.action_identifier,
 
-        parent_action_identifier=parent_action_identifier,
+        parent_reference_identifier=parent_reference_identifier,
         parent_action_item=action_item.parent_action_item,
-
 
         href=href,
         last_updated_text=last_updated_text,
         name=action_item.action_type.name,
 
-        reference_model_name=reference_model_cls._meta.verbose_name,
-        reference_model_url=reference_model_url,
-        reference_model_obj=reference_model_obj,
+        reference_model_name=action_item.reference_model_cls()._meta.verbose_name,
+        reference_url=reference_url,
+        reference_obj=reference_obj,
         action_item_color=action_cls.color_style,
 
-        parent_model_name=parent_reference_model_name,
-        parent_model_url=parent_reference_model_url,
+        parent_reference_model_name=parent_reference_model_name,
+        parent_reference_url=parent_reference_url,
+
+        related_reference_url=related_reference_url,
+        related_reference_model_name=related_reference_model_name,
+        related_reference_identifier=related_reference_identifier,
 
         priority=action_item.priority or '',
         status=action_item.get_status_display(),
         tabindex=tabindex,
         strike_thru=strike_thru)
+
+    return context
