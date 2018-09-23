ac565ae8fb29bc199453afbf02c566f131c5eeb8
erikvw <ew2789@gmail.com>
2018-09-23 12:11:32 -0500

    apps.has_exportable_data

--- edc_action_item/apps.py
+++ edc_action_item/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig as DjangoApponfig
 class AppConfig(DjangoApponfig):
     name = 'edc_action_item'
     verbose_name = 'Action Items'
+    has_exportable_data = True
 
     def ready(self):
         from .signals import action_on_post_delete
