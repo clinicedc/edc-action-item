1cab8e243964b35b8e670742943b8ec29fde5c42
erikvw <ew2789@gmail.com>
2019-04-08 17:21:53 -0500

    gitignore

--- runtests.py
+++ runtests.py
@@ -34,7 +34,6 @@ DEFAULT_SETTINGS = DefaultTestSettings(
         "rest_framework.authtoken",
         "simple_history",
         "edc_auth.apps.AppConfig",
-        "edc_base.apps.AppConfig",
         "edc_protocol.apps.AppConfig",
         "edc_identifier.apps.AppConfig",
         "edc_device.apps.AppConfig",
