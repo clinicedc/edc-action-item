dbacc2a84c1e54bf782f2924726291b6920b4d89
erikvw <ew2789@gmail.com>
2019-06-04 14:24:30 -0500

    add modeladmin mixin to set fields readonly

--- edc_action_item/__init__.py
+++ edc_action_item/__init__.py
@@ -5,4 +5,5 @@ from .create_action_item import create_action_item, SingletonActionItemError
 from .decorators import register
 from .delete_action_item import delete_action_item, ActionItemDeleteError
 from .fieldsets import action_fieldset_tuple, action_fields
+from .modeladmin_mixins import ModelAdminActionItemMixin
 from .site_action_items import site_action_items
