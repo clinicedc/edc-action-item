457b326116efc4027edc9345506c4e53fc1f8c89
erikvw <ew2789@gmail.com>
2020-01-18 07:17:03 -0600

    filter open action items as NEW, OPEN

--- edc_action_item/view_mixins/action_item_view_mixin.py
+++ edc_action_item/view_mixins/action_item_view_mixin.py
@@ -1,5 +1,5 @@
 from django.views.generic.base import ContextMixin
-from edc_constants.constants import CLOSED, CANCELLED
+from edc_constants.constants import NEW, OPEN
 
 from ..model_wrappers import ActionItemModelWrapper
 from ..models import ActionItem
@@ -17,14 +17,11 @@ class ActionItemViewMixin(ContextMixin):
     @property
     def open_action_items(self):
         """Returns a list of wrapped ActionItem instances
-        where status is not OPEN.
+        where status is NEW or OPEN.
         """
-        qs = (
-            ActionItem.on_site.filter(
-                subject_identifier=self.kwargs.get("subject_identifier"),
-                action_type__show_on_dashboard=True,
-            )
-            .exclude(status__in=[CLOSED, CANCELLED])
-            .order_by("-report_datetime")
-        )
+        qs = ActionItem.on_site.filter(
+            subject_identifier=self.kwargs.get("subject_identifier"),
+            action_type__show_on_dashboard=True,
+            status__in=[NEW, OPEN],
+        ).order_by("-report_datetime")
         return [self.action_item_model_wrapper_cls(model_obj=obj) for obj in qs]
