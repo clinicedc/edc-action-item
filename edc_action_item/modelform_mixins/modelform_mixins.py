57c15e87efae25382baff897ec56941bcaac0adf
erikvw <ew2789@gmail.com>
2023-11-28 22:01:11 -0600

    get subject identifier from methed/property

--- edc_action_item/modelform_mixins/modelform_mixins.py
+++ edc_action_item/modelform_mixins/modelform_mixins.py
@@ -10,9 +10,9 @@ class ActionItemModelFormMixin:
         if self.action_cls:
             try:
                 action_cls(
-                    subject_identifier=cleaned_data.get("subject_identifier"),
-                    action_identifier=cleaned_data.get("action_identifier"),
-                    related_action_item=cleaned_data.get("related_action_item"),
+                    subject_identifier=self.subject_identifier,
+                    action_identifier=self.action_identifier,
+                    related_action_item=self.related_action_item,
                     readonly=True,
                 )
             except ActionError as e:
@@ -28,3 +28,23 @@ class ActionItemModelFormMixin:
         except AttributeError:
             action_cls = None
         return action_cls
+
+    @property
+    def action_identifier(self):
+        action_identifier = self.cleaned_data.get("action_identifier")
+        if not action_identifier:
+            try:
+                action_identifier = self.instance.action_identifier
+            except AttributeError:
+                action_identifier = None
+        return action_identifier
+
+    @property
+    def related_action_item(self):
+        related_action_item = self.cleaned_data.get("related_action_item")
+        if not related_action_item:
+            try:
+                related_action_item = self.instance.related_action_item
+            except AttributeError:
+                related_action_item = None
+        return related_action_item
