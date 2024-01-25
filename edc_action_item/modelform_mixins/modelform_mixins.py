3544ee1465265bfc6049cce0c20126ceb7fde858
erikvw <ew2789@gmail.com>
2024-01-24 22:25:05 -0600

    move auth constants to constants module. fix aetmg model form mixin missing aemodelformmixin

--- edc_action_item/modelform_mixins/modelform_mixins.py
+++ edc_action_item/modelform_mixins/modelform_mixins.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from django import forms
+from edc_sites.exceptions import InvalidSiteForSubjectError
+from edc_sites.utils import valid_site_for_subject_or_raise
 
 from ..exceptions import ActionError
 
@@ -14,6 +16,7 @@ if TYPE_CHECKING:
 class ActionItemModelFormMixin:
     def clean(self):
         cleaned_data = super().clean()
+        self.valididate_site_for_subject_or_raise()
         action_cls = self._meta.model.get_action_cls()
         if self.action_cls:
             try:
@@ -44,3 +47,12 @@ class ActionItemModelFormMixin:
         return self.cleaned_data.get("related_action_item") or getattr(
             self.instance, "related_action_item", None
         )
+
+    def valididate_site_for_subject_or_raise(self):
+        if self.get_subject_identifier():
+            try:
+                valid_site_for_subject_or_raise(self.get_subject_identifier())
+            except InvalidSiteForSubjectError:
+                raise forms.ValidationError(
+                    "Subject is not from this site. Please login to the correct site."
+                )
