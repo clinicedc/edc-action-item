1a49a4fd499432630668c50a79c1d4d8d9205fbd
erikvw <ew2789@gmail.com>
2024-03-27 14:48:26 -0500

    update import path from edc-dashboard

--- edc_action_item/tests/urls.py
+++ edc_action_item/tests/urls.py
@@ -1,5 +1,5 @@
 from django.urls.conf import include, path
-from edc_dashboard import url_names
+from edc_dashboard.url_names import url_names
 
 from edc_action_item.admin_site import edc_action_item_admin
 
